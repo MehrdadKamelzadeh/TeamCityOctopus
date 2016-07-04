fcf70091c384fc4b83774ca42907cc19d404b2a2
mehrdadkamelzadeh <mehrdad.kamelzadeh@gmail.com>
2016-07-04 09:50:06 +0430

    First

---
+++ OctopusTeamcityModise/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OctopusTeamcityModise.MvcApplication" Language="C#" %>
---
+++ OctopusTeamcityModise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OctopusTeamcityModise.Startup))]
+namespace OctopusTeamcityModise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
